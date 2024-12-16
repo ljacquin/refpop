69c9818a341cba14ae9e8b75cf9c945f1ab0f703
ljacquin <jacquin.julien@gmail.com>
2024-12-16 18:57:38 +0100

    modify: update readme and minor changes to scripts

--- execute_refpop_tasks_and_analyses.sh
+++ execute_refpop_tasks_and_analyses.sh
@@ -8,12 +8,13 @@ cd src/refpop_data_treatment_and_analysis/
 R -q --vanilla < refpop_0_raw_phenotype_data_manage_type_correction.R
 R -q --vanilla < refpop_1_raw_phenotype_data_outlier_detection_per_env.R
 R -q --vanilla < refpop_2_raw_phenotype_data_spat_hetero_correct_and_h2_estim.R
-R -q --vanilla < refpop_3_adjusted_ls_mean_phenotypes.R
+R -q --vanilla < refpop_3_adjusted_blups_lsmeans_phenotypes_computation.R
 
 # refpop_data_structure_analysis tasks and analyses
 cd ../refpop_data_structure_analysis/
 R -q --vanilla < refpop_genomic_data_structure_analysis.R
 R -q --vanilla < refpop_pedigree_and_phenotype_data_structure_analysis.R
+R -q --vanilla < refpop_gem_data_structure_analysis.R
 
 # refpop_wiser_genomic_prediction_and_analysis tasks and analyses
 cd ../refpop_wiser_genomic_prediction_and_analysis/
